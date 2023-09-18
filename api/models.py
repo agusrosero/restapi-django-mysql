9abc12f8c45e44709fa5b617c62ea80dbaf54a33
agusrosero <agusrosero2000@gmail.com>
2023-09-18 19:35:20 -0300

    start documentation

--- api/models.py
+++ api/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Company(models.Model):
+    
     name = models.CharField(max_length=50)
     website = models.URLField(max_length=100)
     foundation = models.PositiveIntegerField()
 
 
 class Club(models.Model):
+    """
+    Model principal.
+    """
 
     name = models.CharField(max_length=100)
     club_colors = models.CharField(max_length=100)
