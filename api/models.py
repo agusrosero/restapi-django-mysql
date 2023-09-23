56e054c4e14f9690fa9e2ca0ced349438c470e97
agusrosero <agusrosero2000@gmail.com>
2023-09-23 10:37:16 -0300

    documentation

--- api/models.py
+++ api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Create your models here.
 class Company(models.Model):
     
+    """
+    Model test.
+    """
     name = models.CharField(max_length=50)
     website = models.URLField(max_length=100)
     foundation = models.PositiveIntegerField()
